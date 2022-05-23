4613bd88674a53870a90422d27d71d43314294f3
B6122898 <B6122898@g.sut.ac.th>
2022-05-23 13:00:22 +0700

    update error

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -4,18 +4,12 @@ import Dot from './dot.svg'
 import './Posts.css'
 
 
-const Posts = ({ posts, loading , searchText }) => {
+const Posts = ({ posts, loading  }) => {
     
   if (loading) {
     return <h2>Loading...</h2>;
   }
-  const filteredplace= posts.filter(
-    posts => {
-      return (
-        posts.name.toLowerCase().includes(searchText.toLowerCase())  
-      );
-    }
-  );
+ 
 
   return (
      
@@ -25,7 +19,7 @@ const Posts = ({ posts, loading , searchText }) => {
             <div className='grid-item' key={record.id}>
           <div className='card-body'>
             <div className='img-box'>
-              <img src={record.profile_image_url} alt="" />
+              <img src={record.profile_image_url ? record.profile_image_url : ''} alt="" />
             </div>
             <div className='content-box'>
               <p>{record.name}</p>
@@ -33,7 +27,7 @@ const Posts = ({ posts, loading , searchText }) => {
               {record.operation_time.filter(dayone => dayone.day === "Monday").map(data => {
                 return (
                   <span>
-                <img src={Vector} alt="" />
+                <img src={Vector ? Vector : ''} alt="" />
                 {data.time_open} - {data.time_close}
                
                
@@ -42,20 +36,20 @@ const Posts = ({ posts, loading , searchText }) => {
                 )
               })}
               <span className='score'>
-                <img src={Dot} />
-                 {record.rating}
+                <img src={Dot ? Dot : ""}  alt=''/>
+                 {record.rating ? record.rating : "No Ratiing"}
                 </span>
             </div>
           </div>
           <div className="item-images">
             <div className='img-box1'>
-              <img src={record.images[0]} />
+              <img src={record.images[0] ? record.images[0] : ''} alt=''/>
             </div>
             <div className='img-box2'>
-            <img src={record.images[1]} />
+              <img src={record.images[1] ? record.images[1] : ''} alt=''/>
             </div>
             <div className='img-box3'>
-            <img src={record.images[2]} />
+              <img src={record.images[2] ? record.images[2] : ''} alt=''/>
             </div>
           </div>
         </div>
